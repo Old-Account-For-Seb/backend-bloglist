6be2dfdd85868f31c26e90107bc42e47a28fc62a
Sebastian-Sinclair <seb.lucas.sinclair@gmail.com>
2024-05-15 15:19:51 -0400

    4.8 finished

---
+++ tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const { test, after, beforeEach, describe } = require("node:test");
+const assert = require("node:assert");
+const Blog = require("../models/blog");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "first blog",
+    author: "tom holland",
+    url: "lovestolaugh.com",
+    likes: 93,
+  },
+  {
+    title: "second blog",
+    author: "zendaya",
+    url: "lovestosmile.com",
+    likes: 77,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all notes are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, initialBlogs.length);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
