03148ef70d032389d8bdfaf842d57311b5e8271f
Sebastian-Sinclair <seb.lucas.sinclair@gmail.com>
2024-05-23 18:30:49 -0400

    4.15 finished

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const config = require("./utils/config");
 
 mongoose.connect(config.MONGODB_URL);
@@ -10,5 +12,7 @@ mongoose.connect(config.MONGODB_URL);
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,6 +1,9 @@
 const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const helper = require("./test_helper");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
@@ -109,6 +112,39 @@ test("updated blog successfully", async () => {
 
   assert(!titles.includes(blogToUpdate.title));
   assert(titles.includes(newBlog.title));
+});
+
+describe("when there is initially one user in db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "mluukkai",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
 });
 
 after(async () => {
